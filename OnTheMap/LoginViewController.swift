6b58b6809454c48fb8d1144785431ca378093e7f
Maro <maro@mac-maro.gmv.es>
2018-01-26 11:41:11 +0100

    Added launchScreen and login view

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnTheMap
 //
 //  Created by Maro on 25/1/18.
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    @IBOutlet var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    private func styling() {
+        loginButton.backgroundColor = UIColor.udacityBlue
+    }
 }
 
---
+++ OnTheMap/View/ViewConstants.swift
@@ -0,0 +1,15 @@
+//
+//  ViewConstants.swift
+//  OnTheMap
+//
+//  Created by Maro on 26/1/18.
+//  Copyright © 2018 Maro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let udacityBlue = UIColor(red:2.0/255.0 , green:180.0/255.0 , blue:228.0/255.0, alpha:1.0)
+
+}
